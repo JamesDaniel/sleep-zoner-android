65bdd290b4537e54f62e4759aaba917f7bdbd80e
JamesDaniel <jamesmcgarr@protonmail.com>
2017-07-22 00:28:27 +0100

    added interface components

--- app/src/main/java/com/sleep_zoner/sleepzoner/MainActivity.java
+++ app/src/main/java/com/sleep_zoner/sleepzoner/MainActivity.java
@@ -2,12 +2,39 @@ package com.sleep_zoner.sleepzoner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etName, etProfession;
+    private TextView txvName, txvProfession;
+    private Switch pageColorSwitch;
+    private LinearLayout pageLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etProfession = (EditText) findViewById(R.id.etProfession);
+        txvName = (TextView) findViewById(R.id.txvName);
+        txvProfession = (TextView) findViewById(R.id.txvProfession);
+        pageColorSwitch = (Switch) findViewById(R.id.pageColorSwitch);
+        pageLayout = (LinearLayout) findViewById(R.id.pageLayout);
+
+    }
+
+    public void saveAccountData(View view) {
+    }
+
+    public void loadAccountData(View view) {
+    }
+
+    public void openSecondActivity(View view) {
     }
 }
---
+++ app/src/main/java/com/sleep_zoner/sleepzoner/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.sleep_zoner.sleepzoner;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by user on 7/22/17.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView txvName, txvProfession;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        txvName = (TextView) findViewById(R.id.txvName);
+        txvProfession = (TextView) findViewById(R.id.txvProfession);
+    }
+
+    public void loadAccountData(View view) {
+    }
+
+    public void clearAccountData(View view) {
+    }
+
+    public void removeProfessionKey(View view) {
+    }
+}
