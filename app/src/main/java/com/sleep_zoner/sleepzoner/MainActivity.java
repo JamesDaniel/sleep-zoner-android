5a5f6a5babdda8a77dee5f7728b2aac0b92d84b0
JamesDaniel <jamesmcgarr@protonmail.com>
2017-07-23 03:32:20 +0100

    added google login to app

--- app/src/main/java/com/sleep_zoner/sleepzoner/MainActivity.java
+++ app/src/main/java/com/sleep_zoner/sleepzoner/MainActivity.java
@@ -1,13 +1,69 @@
 package com.sleep_zoner.sleepzoner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity
+        implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener
+{
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private SignInButton signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn = (SignInButton) findViewById(R.id.sign_in_button);
+        signIn.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    private void signIn(View view) {
+        Toast.makeText(this, "Hello login", Toast.LENGTH_SHORT).show();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn(v);
+                break;
+
+        }
     }
 }
---
+++ docs/security.md
@@ -0,0 +1,5 @@
+# Instructions on setting up security stuff
+
+## Generate Signing Certificate
+
+ - https://www.akadia.com/services/ssh_test_certificate.html
